a39f1b60b60096548dee2f66f41a1ef1d51452fa
lisal00 <143776518+lisal00@users.noreply.github.com>
2023-11-12 16:32:58 -0600

    Update main.cpp

--- Q2/main.cpp
+++ Q2/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
     ME::myque<int> queue1;
     
-    cout << "Currently, is the queue empty? " << (queue1.empty() ? "Yes" : "No");
+    cout << "Currently, is the queue empty? " << (queue1.empty() ? "Yes." : "No.");
     queue1.push(11);
     queue1.push(22);
     queue1.push(33);
@@ -16,5 +16,6 @@ int main(){
     queue1.pop();
     cout << "Popped the first element. Now the front is: " << queue1.front() << endl;
     cout << "Size of queue is: " << queue1.size();
+    cout << "\nCurrently, is the queue empty? " << (queue1.empty() ? "Yes." : "No.");
     return 0;
 }
